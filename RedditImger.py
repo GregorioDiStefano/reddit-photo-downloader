742d1a177e276ca6fa393f22ca0ce43dc40efa8f
Greg <greg.distefano+github@gmail.com>
2015-09-21 23:07:37 +0200

    Improvements

--- RedditImger.py
+++ RedditImger.py
@@ -3,29 +3,64 @@ import praw
 import re
 from Queue import Queue
 from threading import Thread, Lock
-import time
-import pprint
-
-
+import os
+import hashlib
+import urllib3
+import cPickle
 
 """
     Prevent useless HTTP req. and API uses if a certain item has been seen
     before.
 """
 class Cacher(object):
+    data = []
+    realm = ""
+    fn = ""
+
     def __init__(self, realm):
-        pass
+        try:
+            self.realm = realm
+            self.fn = self.realm + ".p"
+            f = open(self.fn)
+        except Exception, e:
+            self.data = []
+        else:
+            self.data = cPickle.load(open(self.fn, "rb"))
 
     def set(self, url=""):
-        pass
+        self.data.append(url)
 
-    def is_new(self, url=""):
-        pass
+    def exists(self, url=""):
+        return url in self.data
+
+    def check_and_set(self, url=""):
+        if not self.exists(url):
+            self.set(url)
+            return False
+        return True
+
+    def save_cache(self):
+        print self.data
+        cPickle.dump(self.data, file(self.realm + ".p", "wb"))
 
 class ThreadedDownloader(object):
     num_fetch_threads = 10
     queue = Queue()
     mutex = Lock()
+    sr_dirs_created = []
+    download_dir = ""
+
+    def __init__(self, dl_dir="images"):
+        self.download_dir = dl_dir
+        if not os.path.exists(dl_dir):
+            os.mkdir(dl_dir)
+
+    def setup_dir(self, sr):
+        if sr not in self.sr_dirs_created:
+            download_dir_sr = self.download_dir + "/" + sr
+            if not os.path.exists(download_dir_sr):
+                os.mkdir(download_dir_sr)
+                self.sr_dirs_created.append(sr)
 
     def set_queue(self, queue):
         self.queue = queue
@@ -37,16 +72,33 @@ class ThreadedDownloader(object):
             worker.setDaemon(True)
             worker.start()
         self.queue.join()
+        print "Done!"
 
     def work(self):
         print "Starting thread!", self.queue.qsize()
-        while True:
+
+        while self.queue.qsize():
+
+            print "Size: ", self.queue.qsize()
 
             self.mutex.acquire()
             dl = self.queue.get()
             self.mutex.release()
 
-            time.sleep(1)
+            url = dl.values()[0]
+            sr = dl.values()[1]
+
+            self.setup_dir(sr)
+
+            http = urllib3.PoolManager()
+            img_data = http.request("GET", url).data
+            image_hash = hashlib.md5(img_data).hexdigest()
+
+            filename = self.download_dir + "/" + sr + "/" + image_hash
+
+            img_file = open(filename, 'wb')
+            img_file.write(img_data)
+            img_file.close()
 
             self.queue.task_done()
 
@@ -55,19 +107,20 @@ class SRImages(object):
     subreddit = None
     image_urls = []
     reddit_client = None
-    downloader = ThreadedDownloader()
+    downloader = None
     enclosure_queue = Queue()
 
     def __init__(self, rc, subreddit="", limit=100):
         self.subreddit = subreddit
         self.limit = limit
         self.reddit_client = rc
+        self.image_urls = []
 
     def image_ext(self, url):
         return re.match("([^\s]+(\.(?i)(jpg|png))$)", url)
 
     def get_image_urls(self):
-        submissions = self.reddit_client.get_subreddit(self.subreddit).get_hot(limit=100)
+        submissions = self.reddit_client.get_subreddit(self.subreddit).get_hot(limit=self.limit)
         for s in submissions:
             if self.image_ext(s.url) and s.url not in self.image_urls:
                 self.image_urls.append(s.url)
@@ -76,47 +129,43 @@ class SRImages(object):
         return "<%s, images: %d>" % (self.subreddit, len(self.image_urls))
 
 class RedditImger(object):
-    imgur_client = None
     reddit_client = None
 
     cacher = Cacher("reddit_url")
     SRImgsObjs = []
-    subreddit_images = {}
 
-    downloader = ThreadedDownloader()
+    reddit_settings = {}
+    downloader = None
     enclosure_queue = Queue()
 
-    def __init__(self, imgur_settings={}, reddit_settings = {}, subreddits=[]):
+    def __init__(self, imgur_settings={}, reddit_settings={}, subreddits=[]):
 
         try:
-            self.imgur_client_id = imgur_settings["imgur_client_id"]
-            self.imgur_client_secret = imgur_settings["imgur_client_secret"]
-        except KeyError:
-            raise KeyError("Did you set the imgur_settings correctly?")
-
-        try:
-            self.imgur_client = ImgurClient(self.imgur_client_id, self.imgur_client_secret)
+            self.reddit_settings = reddit_settings
             self.reddit_client = praw.Reddit(user_agent=reddit_settings["user_agent"])
+            self.downloader = ThreadedDownloader(reddit_settings["download_dir"])
+            self.subreddits = subreddits
         except KeyError:
             raise KeyError("Did you set the reddit_settings correctly?")
 
+    def start(self):
         # Process every subreddit, get the images, and add the object to a list
-        for subreddit in subreddits:
-            sri = SRImages(self.reddit_client, subreddit, limit = reddit_settings["limit_per_subreddit"])
+        for subreddit in self.subreddits:
+            sri = SRImages(self.reddit_client, subreddit, limit = self.reddit_settings["limit_per_subreddit"])
             sri.get_image_urls()
             self.SRImgsObjs.append(sri)
-
 
         # Add the images to a mapping
         for sri in self.SRImgsObjs:
             sr = sri.subreddit
 
-            self.subreddit_images[sr] = sri.image_urls
             for img_url in sri.image_urls:
-                pair = {"subreddit": sr, "url" : img_url}
-                self.enclosure_queue.put({"subreddit" : sr, "url" : img_url})
+                if not self.cacher.check_and_set(img_url):
+                    pair = {"subreddit": sr, "url": img_url}
+                    self.enclosure_queue.put(pair)
 
         self.downloader.set_queue(self.enclosure_queue)
         self.downloader.start()
+        self.cacher.save_cache()
 
 
--- tests.py
+++ tests.py
@@ -2,6 +2,15 @@ from imgurpython import ImgurClient
 import praw
 import RedditImger
 import unittest
+import random
+import string
+import os
+
+class Utils(object):
+
+    @staticmethod
+    def random_fn():
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
 
 class MyTests(unittest.TestCase):
@@ -13,20 +22,14 @@ class MyTests(unittest.TestCase):
 
     reddit_settings = {
         "user_agent": "Chrome",
-        "limit_per_subreddit" : 100,
+        "limit_per_subreddit" : 50,
+        "download_dir" : "pictures",
     }
 
     subreddit = ["ImagesOfEarth", "SeaPorn"]
 
     def setUp(self):
         pass
-
-    def test_create_imgur_client(self):
-        ri0 = RedditImger.RedditImger(self.imgur_settings,
-                                     self.reddit_settings,
-                                     self.subreddit)
-
-        self.assertIsInstance(ri0.imgur_client, ImgurClient)
 
     def test_create_reddit_client(self):
         ri1 = RedditImger.RedditImger(self.imgur_settings,
@@ -42,6 +45,37 @@ class MyTests(unittest.TestCase):
         self.assertFalse(sri.image_ext("a.jpg/a"))
         self.assertTrue(sri.image_ext("http://reddit.com/file.png"))
         self.assertFalse(sri.image_ext("http://google.ca/"))
+
+    def test_cache_1(self):
+        a = RedditImger.Cacher(Utils.random_fn())
+        self.assertFalse(a.exists("a fake item"))
+        a.set("a fake item")
+        self.assertTrue(a.exists("a fake item"))
+
+    def test_cache_2(self):
+        random_fn = Utils.random_fn()
+        a = RedditImger.Cacher(random_fn)
+        self.assertFalse(a.exists("a fake item"))
+        a.set("a fake item")
+        self.assertTrue(a.exists("a fake item"))
+        a.save_cache()
+
+        b = RedditImger.Cacher(random_fn)
+        self.assertTrue(b.exists("a fake item"))
+        os.remove(random_fn + ".p")
+
+    def test_cache_3(self):
+        random_fn = Utils.random_fn()
+        a = RedditImger.Cacher(random_fn)
+        self.assertFalse(a.check_and_set("a"))
+        self.assertTrue(a.check_and_set("a"))
+
+    def test_reddit_client(self):
+        ri1 = RedditImger.RedditImger(self.imgur_settings,
+                                     self.reddit_settings,
+                                     self.subreddit)
+
+        ri1.start()
 
 if __name__ == '__main__':
     unittest.main()
